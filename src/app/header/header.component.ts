02c14f7483328a82f80a1e98f8c245e9ffe6f5b3
WeijianYuan <weijian.yuan@thentia.com>
2020-03-19 12:17:39 -0400

    save token

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,6 +17,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuth();
     this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
       this.userIsAuthenticated = isAuthenticated;
     });
